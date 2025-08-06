e23cf83d89433900eea9a513a3474b1cd8cc8801
Alexander <alexandersilva580@outlook.com>
2025-08-05 20:23:08 -0400

    creacion del contenido de splash screen con imagen texto e icono

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -12,6 +12,40 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Material();
+    return Material(
+      // Material tiene un unico hijo
+      child: Container(
+        height: MediaQuery.of(context).size.height, // Establecemos la altura
+        width: MediaQuery.of(context).size.width, // Establecemos el ancho
+        decoration: BoxDecoration(
+          color: Colors.black, // se usa mayuscula por que llamamos al paquete Colors
+          image: DecorationImage(
+            image: AssetImage("../images/empanada.png"), // llamamos a la imagen dentro de la carpeta images
+            fit: BoxFit.cover, // La imagen cubrira el area
+            opacity: 0.4, // Opacidad para la imagen semi transferencia
+          ),
+        ),
+        // Column ya puede tener contener a varios
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, // Alineacion de los elementos
+          // Children es lo que contiene los hijos de Column
+          children: [
+            Icon(Icons.shopping_bag_rounded,
+              size: 250,
+              color: Colors.white70,
+            ),
+            Text(
+              "Shopping Empanada",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
